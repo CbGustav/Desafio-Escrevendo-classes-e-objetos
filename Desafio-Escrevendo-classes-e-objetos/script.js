49cdd50ca68d5fb64a7e951a58335ac7f1fe0384
Gustavo Cardoso <112032646+CbGustav@users.noreply.github.com>
2024-06-26 13:40:04 -0300

    Add files via upload

---
+++ Desafio-Escrevendo-classes-e-objetos/script.js
@@ -0,0 +1,44 @@
+class Heroi {
+    constructor (nome, idade, tipo){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+    }
+
+    atacar() {
+        let ataque = ""
+
+        switch (this.tipo.toLowerCase()){
+            case "mago":
+                ataque = "magia"
+                break
+
+                case "Guerreiro":
+                    ataque = "espada"
+                    break
+                
+                    case "monge":
+                        ataque = "artes Marciais"
+                        break
+
+                        case "ninja":
+                        ataque = "shuriken"
+                        break
+
+                        default:
+                            ataque = "ataque desconhecido!"
+                            break
+        }
+
+        console.log(`O ${this.tipo} atacou usando ${ataque}`)
+    }
+}
+const heroi1 = new Heroi("Aragorn", 87, "guerreiro");
+const heroi2 = new Heroi("Gandalf", 2019, "mago");
+const heroi3 = new Heroi("Bruce Lee", 32, "monge");
+const heroi4 = new Heroi("Naruto", 17, "ninja");
+
+heroi1.atacar()
+heroi2.atacar()
+heroi3.atacar()
+heroi4.atacar()
